1a030dbe303bb6b3c1d144802024d6f41773950c
Patrick Ayoup <patrick.ayoup@gmail.com>
2015-01-06 16:00:42 +0000

    setting up test fixture

--- tests/nmf_converter_test.py
+++ tests/nmf_converter_test.py
@@ -11,9 +11,9 @@ class NMFConverterTest(unittest.TestCase):
       Tests the commandline parser.
       No output file is specified.
       """
-      args = nmf_converter.parse_cl_args(['input.midi'])
+      args = nmf_converter.parse_cl_args(['input.mid'])
 
-      assert args.input_file == 'input.midi'
+      assert args.input_file == 'input.mid'
       assert args.output_file is None
 
   def test_parse_cl_args_with_output(self):
@@ -21,27 +21,28 @@ class NMFConverterTest(unittest.TestCase):
       Tests the commandline parser.
       An output file is specified.
       """
-      args = nmf_converter.parse_cl_args(['input.midi', 'output.nmf'])
+      args = nmf_converter.parse_cl_args(['input.mid', 'output.vmf'])
 
-      assert args.input_file == 'input.midi'
-      assert args.output_file == 'output.nmf'
+      assert args.input_file == 'input.mid'
+      assert args.output_file == 'output.vmf'
 
   def test_determine_source_format(self):
       """
       Tests the extraction of file extensions.
       """
-      file_extension = nmf_converter.determine_source_format('/foo/bar/myFile.nmf')
+      file_extension = nmf_converter.determine_source_format('/foo/bar/myFile.vmf')
 
-      assert file_extension == '.nmf'
+      assert file_extension == '.vmf'
 
+  @unittest.skip("vmf file missing")
   def test_convert_score_to_nmf(self):
       """
       Tests the conversion of a score stream to an nmf data structure.
       """
-      score = converter.parse('./fixtures/bwv33.mid')
-      nmf = str(nmf_converter.convert_score_to_nmf(score))
+      score = converter.parse('./tests/fixtures/aus_meines_herz.mid')
+      vmf = str(nmf_converter.convert_score_to_nmf(score))
 
-      with open('./expected/bwv33.nmf', 'r') as expected_file:
+      with open('./tests/expected/aus_meines_herz.vmf', 'r') as expected_file:
           expected = expected_file.read()
 
-          assert expected == nmf
+          assert expected == vmf
