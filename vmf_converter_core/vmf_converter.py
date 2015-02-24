19fb4fcb1fcd618517412b9f53bdff7130cc2ff5
Patrick Ayoup <patrick.ayoup@gmail.com>
2015-02-24 12:57:51 -0500

    Added mechanism to group voices to a single part id. #13

--- vmf_converter_core/vmf_converter.py
+++ vmf_converter_core/vmf_converter.py
@@ -7,7 +7,6 @@ import sys
 import os
 from music21.common import approximateGCD
 
-
 def convert_vmf_to_midi(vmfScore):
     """
     Converts an vmf file to a MIDI file.
@@ -57,28 +56,33 @@ def scan_score_for_largest_chord(score):
 
     return largest_size
 
-def convert_voices_to_parts(score):
+def convert_voices_to_parts(score, id_map):
     """
     Removes polyphonic voices and replaces them with part representations of its voices.
     :param score: Score
+    :param id_map: A mapping of music21 ids to vmf part ids.
     """
     parts_to_insert = {}
+    next_part_id = 0
 
     for i in range(len(score.parts)):
         part = score.parts[i]
 
         if len(list(filter(lambda m: m.hasVoices(), part.elements))) > 0:
-            # record the part id so that we can tie the voices together.
-            part_id = part.id
-
             # break the voices into parts.
             exploded_stream = part.explode()
 
             # assign the part id to all exploded parts.
             # and add the exploded parts to the original stream. Mark where to insert the parts.
             for s in exploded_stream.parts:
-                s.part_id = part_id
+                # all related parts share the same id.
+                id_map[s.id] = next_part_id
                 parts_to_insert[i] = exploded_stream.parts
+        else:
+            # Just record the id.
+            id_map[part.id] = next_part_id
+
+        next_part_id += 1
 
     new_parts = list(score.parts)
 
@@ -128,14 +132,16 @@ def convert_score_to_vmf(score):
     :return: A list of tuples representing the music contained in the MIDI file.
     """
 
+    # Mapping of music21 id to vmf ids
+    id_map = {}
+
     # The smallest duration covered. Expressed as a percentage of a quarter note.
     smallest_note = scan_score_for_shortest_duration(score)
 
     # The number of notes in largest chord in the file.
     largest_chord = scan_score_for_largest_chord(score)
 
-    convert_voices_to_parts(score)
-    #scan_score_for_number_of_voices(score)
+    convert_voices_to_parts(score, id_map)
 
     tie_active = False
 
@@ -160,6 +166,9 @@ def convert_score_to_vmf(score):
                             pitches[-1].append(-1)
                             pitches[-1].append(-1)
 
+                        # Finally, add the part id.
+                        pitches[-1].append(id_map[part.id])
+
                         # a tie can only begin or end at a new note.
                         if element.tie is not None and element.tie.type == 'start':
                             tie_active = True
@@ -172,6 +181,9 @@ def convert_score_to_vmf(score):
                         for i in range(largest_chord - 1):
                             pitches[-1].append(-1)
                             pitches[-1].append(-1)
+
+                        # Add the part id
+                        pitches[-1].append(id_map[part.id])
 
             elif type(element) is chord.Chord:
                 n_frames = element.duration.quarterLength / smallest_note
@@ -191,6 +203,9 @@ def convert_score_to_vmf(score):
                                 current_chord.append(-1)
                                 current_chord.append(-1)
 
+                            # Add the part id.
+                            current_chord.append(id_map[part.id])
+
                             pitches.append(current_chord)
                         else:
                             current_chord = [2, 0, 0]
@@ -204,6 +219,9 @@ def convert_score_to_vmf(score):
                             for i in range(largest_chord - element.multisetCardinality):
                                 current_chord.append(-1)
                                 current_chord.append(-1)
+
+                            # Add the part id.
+                            current_chord.append(id_map[part.id])
 
                             pitches.append(current_chord)
 
@@ -220,11 +238,13 @@ def convert_score_to_vmf(score):
                             current_chord.append(pitch.pitchClass)
                             current_chord.append(pitch.octave)
 
-
                         # Pad remaining note positions for chords smaller than largest.
                         for i in range(largest_chord - element.multisetCardinality):
                             current_chord.append(-1)
                             current_chord.append(-1)
+
+                        # add the part id.
+                        current_chord.append(id_map[part.id])
 
                         pitches.append(current_chord)
 
@@ -232,7 +252,7 @@ def convert_score_to_vmf(score):
                 n_frames = element.duration.quarterLength / smallest_note
 
                 for i in range(int(n_frames)):
-                    pitches.append([0, 0, 0, 0, 0])
+                    pitches.append([0, 0, 0, 0, 0, id_map[part.id]])
 
         parts.append(pitches)
 
