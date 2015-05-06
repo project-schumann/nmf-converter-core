fa65045808c4818651bff3fc244e48930dfe9394
Patrick Ayoup <patrick.ayoup@gmail.com>
2015-05-06 15:42:45 +0000

    Refactored for string parsing

--- vmf_converter_core/vmf_converter.py
+++ vmf_converter_core/vmf_converter.py
@@ -4,16 +4,15 @@ from vmf_converter_core.converter import vmf_converter_core
 
 class VMFConverter(SubConverter):
     """Converter for parsing and writing VMF files."""
-    registerFormats = ('vmf,')
+    registerFormats = ('vmf',)
     registerInputExtensions = ('vmf',)
     registerOutputExtensions = ('vmf',)
 
     def parseData(self, strData, number=None):
         """Parses a string containing VMF data into a music21 stream."""
-        # TODO: Implement Me.
-        pass
+        self.stream = vmf_converter_core.read_vmf_string(strData)
 
     def parseFile(self, filePath, number=None):
         """Parses a file containing VMF data into a music21 stream."""
-        self.stream = vmf_converter_core.read_vmf(filePath)
+        self.stream = vmf_converter_core.read_vmf_file(filePath)
     
