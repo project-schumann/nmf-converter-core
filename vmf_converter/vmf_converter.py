72e4af61df73185554887d208606fd13669d268b
Patrick Ayoup <patrick.ayoup@gmail.com>
2015-05-08 19:11:26 -0400

    added write() method to the subconverter. Currently doesn't work, probably due to a bug in music21. I have inquired on the mailing list about this.

--- vmf_converter/vmf_converter.py
+++ vmf_converter/vmf_converter.py
@@ -6,7 +6,7 @@ class VMFConverter(SubConverter):
     """Converter for parsing and writing VMF files."""
     registerFormats = ('vmf',)
     registerInputExtensions = ('vmf',)
-    registerOutputExtensions = ('vmf',)
+    registerOutputExtensions = ('vmf', '.vmf',)
 
     def parseData(self, strData, number=None):
         """Parses a string containing VMF data into a music21 stream."""
@@ -16,3 +16,8 @@ class VMFConverter(SubConverter):
         """Parses a file containing VMF data into a music21 stream."""
         self.stream = vmf_converter_core.read_vmf_file(filePath)
     
+    def write(self, obj, fmt, fp=None, subformats=None, **keywords):
+        """Writes the music21 stream to a VMF file."""
+        dataStr = vmf_converter_core.convert_score_to_vmf(obj)
+        self.writeDataStream(fp, dataStr)
+        return fp
