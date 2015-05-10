2c173151f4f5301cd30fce12b3a6847bab05ee0a
Patrick Ayoup <patrick.ayoup@gmail.com>
2015-05-10 12:41:56 -0400

    Updated music21 version to use patch fixing converter write(). Finalized integration of write()

--- vmf_converter/vmf_converter.py
+++ vmf_converter/vmf_converter.py
@@ -1,12 +1,14 @@
 """Bi-directional converter for the vector music format (VMF)."""
-from music21.converter.subConverters import SubConverter
+from music21 import converter, note, stream, meter
 import vmf_converter.core.vmf_converter_core as vmf_converter_core
 
-class VMFConverter(SubConverter):
+import json
+
+class VMFConverter(converter.subConverters.SubConverter):
     """Converter for parsing and writing VMF files."""
     registerFormats = ('vmf',)
     registerInputExtensions = ('vmf',)
-    registerOutputExtensions = ('vmf', '.vmf',)
+    registerOutputExtensions = ('vmf',)
 
     def parseData(self, strData, number=None):
         """Parses a string containing VMF data into a music21 stream."""
@@ -18,6 +20,9 @@ class VMFConverter(SubConverter):
     
     def write(self, obj, fmt, fp=None, subformats=None, **keywords):
         """Writes the music21 stream to a VMF file."""
-        dataStr = vmf_converter_core.convert_score_to_vmf(obj)
-        self.writeDataStream(fp, dataStr)
+        vmfDict = vmf_converter_core.convert_score_to_vmf(obj)
+        
+        with open(fp, 'w') as f:
+            f.write(json.dumps(vmfDict))
+        
         return fp
