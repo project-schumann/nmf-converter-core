31592bdcf75e4a09e60848be0024c0f2c331e2f6
Patrick Ayoup <patrick.ayoup@gmail.com>
2015-12-22 21:27:25 -0500

    changed metafiles for version bump

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,4 +1,9 @@
-0.1.0
+0.0.2
+====
+
+-  Updated music21 dependency to a released version.
+
+0.0.1
 =====
 
 -  Initial release
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name='vmf-converter',
   packages=['vmf_converter', 'vmf_converter.core'],
-  version='0.0.1',
+  version='0.0.2',
   description='VMF Converter for Music21',
   long_description = open('README.rst', 'r').read(),
   author = 'Patrick Ayoup',
